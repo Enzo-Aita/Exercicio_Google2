c36cd5add4c5d8f045ff538cb78aed81ae388042
Enzo-Aita <Enzoaitanaime99@gmail.com>
2025-09-29 09:07:48 -0300

    Merge origin/main into main
    # Conflicts:
    #	.gitignore
    #	LICENSE
    #	README.md

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,18 @@
+
+package principal;
+
+
+import javax.swing.JFrame;
+import visao.FrmMenu;
+
+public class Principal {
+    
+    
+    public static void main(String[] args) {
+        JFrame FrmMenu = new FrmMenu();
+        FrmMenu.setVisible(true);
+    }
+}
+
+
+
